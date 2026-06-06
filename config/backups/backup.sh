2038ea3aba6f6691b0bf6555c9c22deb765eded0
Cristian Alexander Conrado Ozonian <132933155+CristianConrado@users.noreply.github.com>
2026-06-07 00:24:45 +0200

    Update backup.sh

--- config/backups/backup.sh
+++ config/backups/backup.sh
@@ -1,9 +1,5 @@
-I want to back up the data of docker images, that is why i saved them all under ~/docker/ so backing them up is easier.
-I decided that one backup a week is more than enough, so I set up rclone to send that backup to my onedrive account, and also saved into a backups folder.
-```bash
+#I want to back up the data of docker images, that is why i saved them all under ~/docker/ so backing them up is easier.
+#I decided that one backup a week is more than enough, so I set up rclone to send that backup to my onedrive account, and also saved into a backups folder.
 
-```
-Should I save the stacks? Well I will because after all this is just for learning.
-```bash
+#Should I save the stacks? Well I will because after all this is just for learning.
 
-```
