7095f015d6c142d01d8f82e1836a38e601c1b2f4
Cristian Alexander Conrado Ozonian <132933155+CristianConrado@users.noreply.github.com>
2026-06-07 12:44:31 +0200

    Update backup.sh

--- config/backups/backup.sh
+++ config/backups/backup.sh
@@ -1,5 +1,32 @@
-#I want to back up the data of docker images, that is why i saved them all under ~/docker/ so backing them up is easier.
-#I decided that one backup a week is more than enough, so I set up rclone to send that backup to my onedrive account, and also saved into a backups folder.
+#I want to back up the data of docker images, that is why i saved them all under ~/docker/ so backing them up is easier. I am also backing up the stacks as it makes it easier to rebuild the homelab I belive.
+#In this version i dont have this automated to be sent, I will do it later
+#!/bin/bash
 
-#Should I save the stacks? Well I will because after all this is just for learning.
+set -euo pipefail
+
+BACKUP_DIR="/tmp/homelab_backups"
+DATE=$(date +%F-%H%M)
+HOST=$(hostname)
+ARCHIVE="${BACKUP_DIR}/${HOST}-${DATE}.tar.zst"
+
+mkdir -p "$BACKUP_DIR"
+
+echo "[$(date)] Creating archive..."
+
+sudo tar \
+  --exclude='*/cache' \
+  --exclude='*/Cache' \
+  --exclude='*/tmp' \
+  --exclude='*/temp' \
+  --exclude='*/transcode' \
+  --exclude='*/transcodes' \
+  --exclude='*/logs' \
+  --zstd \
+  -cf "$ARCHIVE" \
+  /home/cristian/docker/ \
+  /opt/stacks/
+
+echo "[$(date)] Backup complete."
+
+
 
